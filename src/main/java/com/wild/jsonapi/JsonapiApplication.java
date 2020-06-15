ee312dd4c626fcba4b149d6c233c35bdd0e11eea
MrWhite <hugoblanc.blend@gmail.com>
2020-06-15 12:51:12 +0200

    initial working json api with spring

---
+++ src/main/java/com/wild/jsonapi/JsonapiApplication.java
@@ -0,0 +1,13 @@
+package com.wild.jsonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wild/jsonapi/JsonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wild.jsonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
